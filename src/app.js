62799fa75a66b22cb691df5b93d0cef77d59ff10
luisfejaramillor <luisfejaramillor@gmail.com>
2023-04-07 18:34:50 -0500

    updated app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 
 const app = express()
+const PORT = process.env.PORT || 3977
 
-app.get('/', ((req,res)=> {
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+app.get('/', (req,res)=> {
     res.send('holiii')
-}))
+})
 
 
-app.listen(3000)
+app.listen(PORT)
 console.log('Listen on port 3000')
