2821ccc248843a4f1cc91c0397b64a4510c41734
luisfejaramillor <luisfejaramillor@gmail.com>
2023-04-08 19:47:26 -0500

    updated app.js, deleted newfiles

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import { getProducts } from "../utils/readFiles.js";
 import { writeProducts } from "../utils/writeFiles.js";
+import { getProductById } from "../utils/getProductById.js";
+
 // import { products } from "../data/products.txt";
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,16 +17,12 @@ app.use(express.json());
 app.use(express.text());
 
 
-app.get('/products', async(req, res)=> {
+app.get('/api/v1/products', (req, res)=> {
   console.log(products)
   res.json(parsedProducts)
 })
 
-app.get('/products:id', (req, res)=> {
-  res.send('Getting specific data')
-})
-
-app.post('/products', (req, res)=> {
+app.post('/api/v1/products', (req, res)=> {
   parsedProducts.push({id: parsedProducts.length + 1, ...req.body})
   writeProducts(JSON.stringify(parsedProducts))
   res.json(parsedProducts)
